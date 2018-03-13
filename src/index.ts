2bae9a2d0523a3054c338bd0fa223164c470c3aa
j <j.stud.res@gmail.com>
2018-03-13 20:12:14 +0200

    Add ts-loader, html-webpack-plugin and @type/node as dependencies; Configure webpack and tsc; Add index.ts;

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+/// <reference path="../node_modules/p5/lib/p5.d.ts"/>
+
+let p5lib = require("p5");
+
+let s = ( sketch: p5 ) => {
+
+    sketch.setup = () => {
+
+    };
+  
+    sketch.draw = () => {
+
+    };
+  };
+  
+let mySketch = new p5lib(s);
